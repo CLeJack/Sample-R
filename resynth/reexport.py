54584e8d5d79b035bd0fc02721a2a78c01f1045a
CLeJack <lemanjackson.@gmail.com>
2026-03-12 15:13:25 -0500

    adding python resynthesis notebook

---
+++ resynth/reexport.py
@@ -0,0 +1,38 @@
+import os
+from pathlib import Path
+import wave
+import numpy as np
+
+def get_files_in_cwd(path):
+    paths = os.listdir(path)
+    paths = list(map(lambda p: Path(os.getcwd())/ p, paths))
+    return paths
+
+
+def get_files(path):
+    paths = os.listdir(path)
+    paths = list(map(lambda p: Path(path) / p, paths))
+    return paths
+
+
+def export_wavetable(data, path = "./", filename = "output", prepend = "", srate = 44100, channels = 1, sample_width = 2):
+    p = Path(path)
+    filename = prepend+filename+".wav"
+    p = p / filename
+
+    d = data * (0.5 * 2**16 - 1)
+    audio_frames = d.astype(np.int16).tobytes()
+
+    # Write the WAV file
+    with wave.open(str(p), 'wb') as wf:
+        wf.setnchannels(channels)
+        wf.setsampwidth(sample_width)
+        wf.setframerate(srate)
+        wf.writeframes(audio_frames)
+
+    print(f"Successfully wrote {filename}")
+
+def consolidated_export(data, path = "./", filename = "output",srate = 44100, channels = 1, sample_width = 2):
+     wt = [d.frame for d in data]
+     wt = np.array(wt)
+     export_wavetable(wt,path, filename, '', srate, channels, sample_width)
